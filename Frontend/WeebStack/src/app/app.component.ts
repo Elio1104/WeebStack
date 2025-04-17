dfa667071834721284341a79732099d375cc8279
Aaron Londot <a.londot@stag.technofuturtic.education>
2025-04-17 13:40:43 +0200

    responsive navbar + start theme change

--- Frontend/WeebStack/src/app/app.component.ts
+++ Frontend/WeebStack/src/app/app.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {Component, computed, HostListener, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NavbarComponent} from './features/navbar/navbar.component';
+import {NgClass} from '@angular/common';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, NavbarComponent, NgClass, MatSlideToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  isLeftSidebarCollapsed = signal<boolean>(true);
+  screenWidth = signal<number>(window.innerWidth);
   title = 'WeebStack';
+
+  OnInit() : void {
+    this.isLeftSidebarCollapsed.set(true);
+  }
+
+  @HostListener('window:resize')
+  onResize() : void {
+    this.screenWidth.set(window.innerWidth);
+    if (this.screenWidth() < 768) {
+      this.isLeftSidebarCollapsed.set(true);
+    }
+  }
+
+  sizeClass = computed(() => {
+    if (this.isLeftSidebarCollapsed())
+      return '';
+    return this.screenWidth() > 768 ? 'body-trimmed' : 'body-md-screen';
+
+  })
+
+  changeIsLeftSidebarCollapsed(IsLeftSidebarCollapsed : boolean) : void {
+    this.isLeftSidebarCollapsed.set(IsLeftSidebarCollapsed);
+  }
 }
---
+++ Frontend/WeebStack/src/app/features/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import {Component, input, output} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {NgClass, NgFor, NgIf} from '@angular/common';
+import {MatSlideToggle} from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterModule, NgFor, NgClass, NgIf, MatSlideToggle],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  isLeftSidebarCollapsed = input.required<boolean>();
+  changeIsLeftSidebarCollapsed = output<boolean>();
+
+  items = [
+    { label: 'Home', icon: 'fa fa-cube', routeLink: 'home' },
+    { label: 'Drag', icon: 'fa fa-wrench', routeLink: 'drag' },
+  ]
+
+  toggleCollapse() : void {
+    this.changeIsLeftSidebarCollapsed.emit(!this.isLeftSidebarCollapsed());
+  }
+
+  closeSidenav() : void {
+    this.changeIsLeftSidebarCollapsed.emit(true);
+  }
+}
